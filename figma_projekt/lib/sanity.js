6002ad601658ffb4138f5d5522624cc533cc7572
ramizPro <ozbej07@gmail.com>
2026-04-13 16:40:35 +0200

    vercel errors fix

--- figma_projekt/lib/sanity.js
+++ figma_projekt/lib/sanity.js
@@ -2,7 +2,7 @@ import { createClient } from "@sanity/client";
 
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: "production",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,  
   apiVersion: "2024-01-01",
   useCdn: false,
   token: process.env.SANITY_API_TOKEN,
